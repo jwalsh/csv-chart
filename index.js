bc4585372e0bf7f4b1f6ad8af0f05ec2136fe0c6
Jason Walsh <jwalsh@saylent.com>
2017-12-18 11:18:27 -0500

    Merge options with defaults

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
+const defaultOptions = {
+  debug: false,
+  delimiter:',',
+  headerLoc: 0,
+  headerPrefix: '',
+  labelLoc: 0,
+  whitelist: []
+};
+
 var exports = module.exports = (dw,
-                                options = {
-                                  debug: false,
-                                  delimiter:',',
-                                  headerLoc: 0,
-                                  headerPrefix: '',
-                                  labelLoc: 0,
-                                  whitelist: []
-                                }) =>
+                                options = defaultOptions) =>
     {
-      if (options.debug) {
-        console.log(options);
+      const opts = Object.assign({}, defaultOptions, options);
+
+      if (opts.debug) {
+        console.log(opts);
       }
       const table = dw
             .split(/[\r\n]+/)
             .filter(e => e !== '');
 
-      if (options.debug) {
+      if (opts.debug) {
         console.log(table);
       }
 
-      const header = table[options.headerLoc || 0]
-            .split(options.delimiter)
+      const header = table[opts.headerLoc || 0]
+            .split(opts.delimiter)
             .map(e => e.trim()).filter(e => e !== '');
       const data = table
             .splice(1)
             .map(e => e
-                 .split(options.delimiter)
+                 .split(opts.delimiter)
                  .map(e => e.trim())
                  .filter(e => e !== ''));
 
       let chart = { };
 
       chart.labels = data.map(e => {
-        return `${options.headerPrefix !== '' ? options.headerPrefix + ' ' : ''}${e[0]}`;
+        return `${opts.headerPrefix !== '' ? opts.headerPrefix + ' ' : ''}${e[0]}`;
       });
 
       chart.datasets = header
